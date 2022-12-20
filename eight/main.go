fe11bd0239a135a4cc271acafc9d3d6e8e33031a
Jamie Henson <jamie@jamiehenson.com>
2022-12-20 20:20:00 +0000

    DRY'd Day 8

--- eight/main.go
+++ eight/main.go
@@ -8,6 +8,30 @@ type Coordinate struct {
 	x, y int
 }
 
+func makeRange(min, max int, rev bool) []int {
+	a := make([]int, max-min)
+	for i := range a {
+		if rev {
+			a[i] = max - i
+		} else {
+			a[i] = min + i
+		}
+	}
+	return a
+}
+
+func calculateVisibility(tg [][]string, xr []int, yr []int, x int, y int) bool {
+	for _, j := range yr {
+		for _, i := range xr {
+			if tg[i][j] >= tg[x][y] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
 func main(file []byte) int {
 	treesSource := string(file)
 	treeGrid := strings.Split(treesSource, "\n")
@@ -16,8 +40,10 @@ func main(file []byte) int {
 	h := len(treeGrid)
 	v := []Coordinate{}
 
-	treesSource = strings.ReplaceAll(treesSource, "\n", "")
-	treeGrid = strings.Split(treesSource, "")
+	tg := [][]string{}
+	for _, row := range treeGrid {
+		tg = append(tg, strings.Split(row, ""))
+	}
 
 	for y := 0; y < h; y++ {
 		for x := 0; x < w; x++ {
@@ -26,58 +52,31 @@ func main(file []byte) int {
 				continue
 			}
 
-			lv, rv, tv, bv := true, true, true, true
-
-			// left check
-			for cx := y * w; cx < (y*w)+x; cx++ {
-				if treeGrid[cx] >= treeGrid[y*w+x] {
-					lv = false
-					break
-				}
-			}
+			lv := calculateVisibility(tg, makeRange(0, x, false), []int{y}, x, y)
 
 			if lv {
 				v = append(v, Coordinate{x, y})
 				continue
 			}
 
-			// right check
-			for cx := (y+1)*w - 1; cx > y*w+x; cx-- {
-				if treeGrid[cx] >= treeGrid[y*w+x] {
-					rv = false
-					break
-				}
-			}
+			rv := calculateVisibility(tg, makeRange(x, w-1, true), []int{y}, x, y)
 
 			if rv {
 				v = append(v, Coordinate{x, y})
 				continue
 			}
 
-			// top check
-			for cy := x; cy < x+(y*w); cy += w {
-				if treeGrid[cy] >= treeGrid[y*w+x] {
-					tv = false
-					break
-				}
-			}
+			tv := calculateVisibility(tg, []int{x}, makeRange(0, y, false), x, y)
 
 			if tv {
 				v = append(v, Coordinate{x, y})
 				continue
 			}
 
-			// bottom check
-			for cy := x + ((h - 1) * w); cy > x; cy -= w {
-				if treeGrid[cy] >= treeGrid[y*w+x] {
-					bv = false
-					break
-				}
-			}
+			bv := calculateVisibility(tg, []int{x}, makeRange(y, h-1, true), x, y)
 
 			if bv {
 				v = append(v, Coordinate{x, y})
-				continue
 			}
 		}
 	}
